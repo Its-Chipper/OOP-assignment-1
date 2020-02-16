21c957261fcaf8220340ea38abde87b181c14234
TomMoxom <18674258@students.lincoln.ac.uk>
2020-02-16 20:24:28 +0000

    Added methods for the different majorities

--- Console-Implementation/Console-Imp/Console-Imp/Functions.cs
+++ Console-Implementation/Console-Imp/Console-Imp/Functions.cs
@@ -4,9 +4,31 @@ using System.Text;
 
 namespace Console_Imp
 {
-    //this calss will hold all of the functions that are needed for the code
+    //this class will hold all of the functions that are needed for the code
     class Functions
     {
-
+        private bool Majority(float percentOfVote, float percentOfPop, float percentNeed, float popNeeded)
+        {
+            if ((percentOfVote > percentNeed) && (percentOfPop > popNeeded))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        bool SimpMajority(float percentOfVote, float percentOfPop)
+        {
+            return Majority(percentOfVote, percentOfPop, (float)0.5, (float)0.0);
+        }
+        bool ReinforcedMajority(float percentOfVote, float percentOfPop)
+        {
+            return Majority(percentOfVote, percentOfPop, (float)0.72, (float)0.65);
+        }
+        bool QualifiedMajority(float percentOfVote, float percentOfPop)
+        {
+            return Majority(percentOfVote, percentOfPop, (float)0.55, (float)0.65);
+        }
     }
 }
